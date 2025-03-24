404c885358a5f1ad569596c2857b82fdc765e701
Vaughn <vaughng1213@gmail.com>
2025-03-23 23:04:10 -0700

    Hello desktop.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Salesforce 95
 
 Salesforce but it looks and feels like Windows 95.
+
+# Resource Log
+
+A chronological list of resources that were pivotal to the development of this project.
+
+* https://developer.salesforce.com/docs/platform/lwc/guide/use-config-custom-tab.html
+* https://salesforce.stackexchange.com/questions/289793/how-to-trigger-a-click-event-on-lwc-element
+* https://developer.salesforce.com/docs/platform/lwc/guide/create-components-dom-work.html
+* https://github.com/salesforce/lwc/issues/1484
--- force-app/main/default/lwc/desktop/desktop.js
+++ force-app/main/default/lwc/desktop/desktop.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class Desktop extends LightningElement {}
+export default class Desktop extends LightningElement {
+    toggleStartButton() {
+        const startMenuCnt = this.template.querySelector('.start-menu-container');
+        const startCnt = this.template.querySelector('.start-button-container');
+        const startBtn = this.template.querySelector('.start-button');
+        const startBtnCnt = this.template.querySelector('.start-button-content');
+        startMenuCnt.classList.toggle('active');
+        startCnt.classList.toggle('active');
+        startBtn.classList.toggle('active');
+        startBtnCnt.classList.toggle('active');
+    }
+}
